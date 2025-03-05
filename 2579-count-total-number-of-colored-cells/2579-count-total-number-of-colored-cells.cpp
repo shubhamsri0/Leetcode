81a045c7a2c4d2496a3d1c8203ba514351ded6c6
Shubham Srivastava <srivastavshubham321@gmail.com>
2025-03-05 23:31:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.36%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long count=1;
-        long long jump=4;
-        while(n>1) {
-            count+=jump;
-            jump+=4;
-            n--;
-        }
-        return count;
+        // long long count=1;
+        // long long jump=4;
+        // while(n>1) {
+        //     count+=jump;
+        //     jump+=4;
+        //     n--;
+        // }
+        // return count;
+        long long np=n;
+        return 1+2*(np-1)*np;
     }
 };
