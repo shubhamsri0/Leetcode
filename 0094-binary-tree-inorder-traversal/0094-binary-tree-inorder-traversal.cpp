8d9f795b9e092ca821af99c35e5c882d9f864a3b
Shubham Srivastava <srivastavshubham321@gmail.com>
2025-07-07 11:53:31 +0530

    Time: 0 ms (100%), Space: 10.8 MB (63.85%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,37 +11,78 @@
  */
 class Solution {
 public:
+    TreeNode* findPred(TreeNode* curr) {
+        TreeNode* pre = curr->left;  // Start from the left child
+        // Move to the rightmost node of the left subtree
+        while (pre->right != nullptr && pre->right != curr) {
+            pre = pre->right;
+        }
+        return pre;
+    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         
-        // if(!root) {
-        //     return ans;
-        // }
-        // vector<int>left=inorderTraversal(root->left);
-        // ans.insert(ans.end(),left.begin(),left.end());
-        // ans.push_back(root->val);
+//     /*Recursive Approach (TC-O(n);SC-O(n))/*
+
+//         // if(!root) {
+//         //     return ans;
+//         // }
+//         // vector<int>left=inorderTraversal(root->left);
+//         // ans.insert(ans.end(),left.begin(),left.end());
+//         // ans.push_back(root->val);
         
-        // vector<int>right=inorderTraversal(root->right);
-        // ans.insert(ans.end(),right.begin(),right.end());
+//         // vector<int>right=inorderTraversal(root->right);
+//         // ans.insert(ans.end(),right.begin(),right.end());
 
-        // return ans;
+//         // return ans;
 
-        //Iterative Approach
+//     /* Iterative Approach  (TC-O(n);SC-O(n)) */
 
-        stack<TreeNode*>st;
-        TreeNode* curr=root;
+//         // stack<TreeNode*>st;
+//         // TreeNode* curr=root;
 
-        while(curr!=NULL || !st.empty()) {
-            //Left Coverage
-            while(curr!=NULL) {
-                st.push(curr);
-                curr=curr->left;
+//         // while(curr!=NULL || !st.empty()) {
+//         //     //Left Coverage
+//         //     while(curr!=NULL) {
+//         //         st.push(curr);
+//         //         curr=curr->left;
+//         //     }
+//         //     curr=st.top();
+//         //     st.pop();
+//         //     ans.push_back(curr->val);
+//         //     curr=curr->right;
+//         // }
+//         // return ans;
+
+//     /* Moris travesal */
+
+
+        TreeNode* curr = root;
+
+        while (curr != nullptr) {
+            // If left child is NULL, visit the current node and move to the right
+            if (curr->left == nullptr) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                // Find the inorder predecessor of the current node
+                TreeNode* pre = findPred(curr);
+
+                // If the right pointer of the predecessor is NULL, make a temporary link
+                if (pre->right == nullptr) {
+                    pre->right = curr;  // Make curr the right child of the predecessor
+                    curr = curr->left;  // Move to the left child
+                } else {
+                    // If the right pointer of the predecessor points to curr, we have finished the left subtree
+                    pre->right = nullptr;  // Restore the tree structure
+                    ans.push_back(curr->val);  // Visit the current node
+                    curr = curr->right;  // Move to the right child
+                }
             }
-            curr=st.top();
-            st.pop();
-            ans.push_back(curr->val);
-            curr=curr->right;
         }
+
         return ans;
     }
 };
+
+
