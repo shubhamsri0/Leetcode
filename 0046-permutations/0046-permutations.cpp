5601e091e717038452b4c0754dbe1f36fd4399b2
Shubham Srivastava <srivastavshubham321@gmail.com>
2025-08-11 11:22:57 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.76%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void allperm(vector<vector<int>>& ans,vector<int>& nums,int idx) {
+        if(idx == nums.size()) {
+            ans.push_back({nums});
+            return;
+        }
+
+        for(int i=idx;i<nums.size();i++) {
+            swap(nums[idx],nums[i]);
+            allperm(ans,nums,idx+1);
+            swap(nums[idx],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        allperm(ans,nums,0);
+        return ans;
+    }
+};
